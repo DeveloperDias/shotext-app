82bdc329a44c06075a75b4b9d758e6468efe4d73
DelevoperDias <robertodiasdev@gmail.com>
2023-08-17 05:11:45 -0300

    comments added

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
+# Importando bibliotecas
 import pyautogui
 import pytesseract
 import cv2
 import keyboard
-from pyperclip import copy
 import pystray
 import sys
-from PIL import Image
 import os
+from pyperclip import copy
+from PIL import Image
 from plyer import notification
 
-
+# Criando um método que verifica se está aberto em desenvolvimento ou em um executável
+# Ele retorna o local apropriado dependendo de onde o aplicativo se encontra
 def get_resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath(""), relative_path)
 
 
-image_icon_path = get_resource_path(r"assets/icon.png")
-image_icon = Image.open(image_icon_path)
+# Definindo variáveis de caminhos dinâmicos
 screenshot_url = get_resource_path(r"assets/screenshot.png")
+image_icon_path = get_resource_path(r"assets/icon.png")
 
 
+# Criando uma variável de um objeto de Imagem com o caminho selecionado
+image_icon = Image.open(image_icon_path)
+
+
+# Uma classe que referencía a linguagem para qual sera usada no aplicativo
 class Language:
     def __init__(self):
         self.actual_language = None
@@ -29,16 +36,24 @@ class Language:
         self.actual_language = new_actual_language
 
 
+# instancia da classe que sera usada
 app_language = Language()
 
 
-def img_to_string():
-    image = cv2.imread(screenshot_url)
+# método simples para tirar um screenshot da tela inteira do usuário
+def screenshot_from_app():
+    screenshot = pyautogui.screenshot()
+    screenshot.save(screenshot_url)
+
+
+# Método que transforma uma imagem em texto usando tesseract
+def img_to_string(img_url):
     path = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
     pytesseract.pytesseract.tesseract_cmd = path
-    return pytesseract.image_to_string(image, lang=app_language.actual_language)
+    return pytesseract.image_to_string(img_url, lang=app_language.actual_language)
 
 
+# Método para recortar a screenshot e usar o método img_to_string() para copiar o texto da imagem
 def cut_image():
     try:
         im = cv2.imread(screenshot_url)
@@ -47,35 +62,34 @@ def cut_image():
         roi = cv2.selectROI("Select ROI", im, showCrosshair=False)
         im_cropped = im[int(roi[1]):int(roi[1] + roi[3]), int(roi[0]):int(roi[0] + roi[2])]
         cv2.imwrite(screenshot_url, im_cropped)
-        image_in_string = img_to_string()
+        image_in_string = img_to_string(screenshot_url)
+
         if not image_in_string:
+            # se der erro ao transformar a imagem em texto (aparece notificação)
             notification.notify(
                 title="Erro ao copiar imagem",
                 message="Ocorreu um erro ao copiar a imagem, tente novamente (selecione uma imagem com texto)",
                 app_name="Shotext",
                 app_icon=get_resource_path("favicon.ico"),
                 timeout=2,
-                toast=False,
-                ticker="teste"
             )
         else:
+            # Copiar string para a área de transferência do usuário
             copy(image_in_string)
     except Exception as e:
-        copy(f"Error: {e}")
+        print(f"Error: {e}")
     finally:
+        # Fecha todas as janelas caso der certo ou não
         cv2.destroyAllWindows()
 
 
-def screenshot_from_app():
-    screenshot = pyautogui.screenshot()
-    screenshot.save(screenshot_url)
-
-
+# Método principal que faz a captura de tela, recorta a imagem e copía o texto dela para a área de transferência
 def on_press_key():
     screenshot_from_app()
     cut_image()
 
 
+# Método geral do Menu Icon
 def app_handle(icon_app, item):
     if str(item) == "Screenshot":
         on_press_key()
@@ -84,6 +98,7 @@ def app_handle(icon_app, item):
         sys.exit()
 
 
+# Método para mudar a linguagem usada no scanner do aplicativo
 def change_language(_, item):
     if str(item) == "Portuguese":
         app_language.change_actual_language("por")
@@ -91,17 +106,21 @@ def change_language(_, item):
         app_language.change_actual_language("eng")
 
 
+# Sub-menu de linguagens do pystray
 languages_submenu = pystray.Menu(
     pystray.MenuItem("Portuguese", change_language),
     pystray.MenuItem("English", change_language)
 )
 
+
+# Menu principal do pystray
 icon = pystray.Icon("Shotext", image_icon, menu=pystray.Menu(
     pystray.MenuItem("Languages", languages_submenu),
     pystray.MenuItem("Screenshot", app_handle),
     pystray.MenuItem("Exit", app_handle)
 ))
 
+# Atalho para o método principal do aplicativo
 keyboard.add_hotkey("ctrl+[", on_press_key)
 
 if __name__ == "__main__":
